2824960a343c76d95239fd558343af66110e551f
Katrina <katrina.m.hall@gmail.com>
2014-06-02 16:47:56 -0400

    Committing initials for starting database rebuild

--- rsync_commands.py
+++ rsync_commands.py
@@ -1,8 +1,8 @@
 
-for i in range(106, 114) :
+for i in range(109, 114) :
 	command = "rsync -avz --delete --delete-excluded --exclude **/text-versions/ govtrack.us::govtrackdata/congress/" + str(i) + "/votes " + str(i) + " &"
 	print command
 
-for i in range(106, 114) :
+for i in range(109, 114) :
 	command = "rsync -avz --delete --delete-excluded --exclude **/text-versions/ govtrack.us::govtrackdata/congress/" + str(i) + "/bills " + str(i) + " &"
 	print command
